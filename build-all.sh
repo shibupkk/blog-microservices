c394c1a83b51b76049513b766267e2b79ba1520e
shibus29@in.ibm.com <shibus29@in.ibm.com>
2019-07-30 19:36:50 +0530

    First Commit

---
+++ build-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd microservices/core/product-service;                ./gradlew clean publishToMavenLocal; cd -
+cd microservices/core/recommendation-service;         ./gradlew clean publishToMavenLocal; cd -
+cd microservices/core/review-service;                 ./gradlew clean publishToMavenLocal; cd -
+cd microservices/composite/product-composite-service; ./gradlew clean build; cd -
+
+cd microservices/support/discovery-server;            ./gradlew clean build; cd -
+cd microservices/support/edge-server;                 ./gradlew clean build; cd -
+
